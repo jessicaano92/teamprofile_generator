2e06d3a8529de946d02d96c0b3d96420c7b643a3
Jessica Anocibar <jessicayaws@yahoo.com>
2020-10-18 17:06:42 -0600

    added files and dependency

---
+++ index.js
@@ -0,0 +1,18 @@
+//will need inquirer.prompt to make objects of questions to prompt the user for their email, id, and specific information based on their role with the company
+
+//also prompt the user for information about the team manager and then info about team members-can be a mix of engineers and interns
+ 
+
+//in HTML- cards are already made
+
+
+
+
+var inquirer = require("inquirer");
+
+inquirer.prompt ([
+    {
+
+    }
+])
+
